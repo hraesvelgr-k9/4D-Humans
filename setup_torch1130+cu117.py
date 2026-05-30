31e98558c35300566da44f1262ec2cc824bb1dcf
hraesvelgr-k9 <hraesvelgr@ketus-ix.quest>
2026-05-30 14:40:45 +0900

    Add setup.py that pins torch==1.13.0+cu117 to enable installation on SLAHMR as a PHALP dependency

---
+++ setup_torch1130+cu117.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+print('Found packages:', find_packages())
+setup(
+    description='HMR2 as a package',
+    name='hmr2',
+    packages=find_packages(),
+    install_requires=[
+        'gdown',
+        'numpy',
+        'torch==1.13.0+cu117',
+        'torchvision==0.14.0+cu117',
+        'pytorch-lightning==1.9.5',
+        'smplx==0.1.28',
+        'pyrender',
+        'opencv-python',
+        'yacs',
+        'scikit-image',
+        'einops',
+        'timm',
+        'webdataset',
+        'dill',
+        'pandas',
+        'chumpy @ git+https://github.com/mattloper/chumpy',
+    ],
+    extras_require={
+        'all': [
+            'detectron2 @ git+https://github.com/facebookresearch/detectron2',
+        ],
+    },
+)
